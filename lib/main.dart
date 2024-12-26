0d7b4648a6f192891165a38aced6523dd72c66b9
Youssef amr <youssefamr8050@gmail.com>
2024-12-26 16:12:41 +0200

    on_boarding

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+
+import 'onboarding.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,7 +16,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      routes: {},
+      routes: {
+        Onboarding.routeName: (context)=> Onboarding()
+      },
       initialRoute: "/",
     );
   }
